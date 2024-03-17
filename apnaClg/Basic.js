41b37d3b05b842838de2ef88a1bb903ef4993318
Pooja Meena <poojameena21@navgurukul.org>
2024-03-17 20:57:44 +0530

    basic js prectise (alert,promt,object etc.. )

---
+++ apnaClg/Basic.js
@@ -0,0 +1,66 @@
+// / javascript.info ///
+
+
+// alert("hello")
+// [1,2].forEach(alert);
+// ["hello pooja","hello shivani","hello simi :)"].forEach(alert);
+// alert()
+// console.log(alert)
+
+
+
+// alert("hello")[1,2].forEach(alert)
+
+
+
+
+// let arr=[1, 2, 3, 4, 5, 6];
+
+
+// arr.forEach((item)=>{
+//     console.log(item*item)
+// })
+
+
+
+
+// const a =prompt("enter your input"); 
+// arr=[]
+// for(i=1; i<=a; i++){
+//     arr[i-1] =i;
+// }
+// console.log(arr)
+// const sum=arr.reduce((prev,present)=>{
+//     return prev+present
+// })
+// console.log(sum)
+
+
+// const age=23;
+
+
+
+// let name="pooja";
+// alert(`hello ${name}`)
+
+
+
+
+
+//input for js as alert///
+
+
+
+
+// var userInput = prompt("Please enter some text:");
+// if (userInput !== null) {
+//     alert("You entered: " + userInput);
+// } else {
+//     alert("You canceled the input.");
+// }
+
+
+
+
+
+
---
+++ apnaClg/opratore.js
@@ -0,0 +1,81 @@
+// const a =4
+// const b=5
+// let sum;
+// sum=a++
+// console.log(sum)
+// console.log(a+b)
+
+// function kajal(){
+//     return "hello i am kajal and i done my work"
+// }
+
+// function devika(){
+//     return "hello This is devika and i done my work"
+// }
+
+// function parveen(pooja){
+//     return pooja()
+
+// }
+// const gotdata=parveen(devika)
+// console.log(gotdata)
+
+
+
+
+// function anuj() {
+//   return "hello i am anuj";
+
+//   function kajal() {
+//     return "hello i am kajal";
+//   }
+
+//   function devika() {
+//     return "hello i am devika";
+//   }
+// }
+
+// function parveen() {
+//   return "i will return what you will say return me";
+// }
+
+
+
+
+// alert("hello this is my apnclg class")
+// alert("and this is my alert topic is going on")
+
+
+// let name= prompt("write your name");
+// console.log(name)
+
+
+
+// let num = prompt("inter your number");
+// if (num%5===0){
+//   console.log("yes")
+// }else{
+//   console.log("no")
+// }
+
+
+
+// let input=prompt("enter the score");
+
+// if (input>=90 && input<=100){
+//     console.log("gread A");
+// }else if(input>=70 && input<=89){
+//   console.log("gread B")
+// }else if(input>=60 && input<=69){
+//   console.log("gread C")
+// }else if(input>=50 && input<=59){
+//   console.log("gread D")
+// }else if (input>=0 && input<=49){
+//   console.log("gread F")
+// }
+
+
+
+
+
+
